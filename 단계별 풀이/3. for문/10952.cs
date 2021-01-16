1931822b275c4697e9e432d5fb3d126504704c57
H0Kyun <72697808+H0Kyun@users.noreply.github.com>
2021-01-17 03:25:47 +0900

    A+B - 5

---
+++ 단계별 풀이/3. for문/10952.cs
@@ -0,0 +1,25 @@
+// 문제 출처: https://www.acmicpc.net/problem/10952
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+class Program{
+    static void Main(string[] args){
+        int a =0, b =0, c = 0;
+		
+		do{
+        	string[] ab = Console.ReadLine().Split(' ');
+        	a = int.Parse(ab[0]);
+        	b = int.Parse(ab[1]);
+        	c = a + b;
+        	if(c !=0){
+        		Console.WriteLine(c);		    		
+        	}
+       	
+		}while(a != 0 && b != 0);
+
+        
+
+    }
+}
