3c5ed9390d4ce4a2907182bae128318fb952ed19
H0Kyun <72697808+H0Kyun@users.noreply.github.com>
2021-01-17 03:18:25 +0900

    A+B - 3

---
+++ 단계별 풀이/3. for문/10950.kt
@@ -0,0 +1,13 @@
+// 문제 출처: https://www.acmicpc.net/problem/10950
+
+import java.util.Scanner
+
+fun main() = with (Scanner(System.`in`)){
+    var T = nextInt()
+    
+    for(i in 1..T){
+        var A = nextInt()
+        var B = nextInt()
+        println(A+B)
+    }
+}
