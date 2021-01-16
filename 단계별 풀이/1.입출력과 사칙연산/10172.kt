bb432af5b2d7a831498522369e7e64658ecca29a
H0Kyun <72697808+H0Kyun@users.noreply.github.com>
2021-01-17 03:03:38 +0900

    개

--- 단계별 풀이/1.입출력과 사칙연산/10172.kt
+++ 단계별 풀이/1.입출력과 사칙연산/10172.kt
@@ -1,4 +1,4 @@
-// 문제 출처: https://www.acmicpc.net/source/21249693
+// 문제 출처: https://www.acmicpc.net/problem/10172
 
 fun main(){
     print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|")
