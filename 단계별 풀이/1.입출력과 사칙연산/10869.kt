d292afa8b06cc3425cede7dc0479cf5783807c55
H0Kyun <72697808+H0Kyun@users.noreply.github.com>
2021-01-17 03:07:26 +0900

    사칙연산

---
+++ 단계별 풀이/1.입출력과 사칙연산/10869.kt
@@ -0,0 +1,14 @@
+// 문제 출처: https://www.acmicpc.net/problem/10869
+
+import java.util.*
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    val A : Int = nextInt()
+    val B : Int = nextInt()
+    
+    println(A+B)
+    println(A-B)
+    println(A*B)
+    println(A/B)
+    println(A%B)
+}
