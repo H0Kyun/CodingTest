d0403ae4343a7f927ba3be017805500ee2adee1f
H0Kyun <72697808+H0Kyun@users.noreply.github.com>
2021-01-17 03:26:23 +0900

    A+B - 4

---
+++ 단계별 풀이/3. for문/10951.cs
@@ -0,0 +1,24 @@
+// 문제 출처: https://www.acmicpc.net/problem/10951
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+class Program{
+    static void Main(string[] args){
+        int a =0, b =0, c = 0;
+		string ab;
+		string[] spl_ab;
+
+		while((ab = Console.ReadLine()) != null){
+			spl_ab = ab.Split(' ');
+			a = int.Parse(spl_ab[0]);
+        	b = int.Parse(spl_ab[1]);
+        	c = a + b;
+        	Console.WriteLine(c);
+		}
+
+        
+
+    }
+}
