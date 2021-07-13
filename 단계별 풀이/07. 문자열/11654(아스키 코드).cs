0d26b692259995e0bda515d5e94d5261ee92c39e
Hokyun <h0kyungim@gmail.com>
2021-07-14 00:16:02 +0900

    Add 15596 and 11654

---
+++ 단계별 풀이/07. 문자열/11654(아스키 코드).cs
@@ -0,0 +1,15 @@
+//문제 출처: https://www.acmicpc.net/problem/11654
+
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char cnum = Convert.ToChar(Console.ReadLine());
+        int inum = Convert.ToInt32(cnum);
+
+        Console.WriteLine(inum);
+    }
+}
