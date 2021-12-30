2a0c4afe284d736dda91de9fb8a0a01435a7dc16
Hokyun <h0kyungim@gmail.com>
2021-12-30 10:46:15 +0900

    Add 10809

---
+++ 단계별 풀이/07. 문자열/10809(알파벳 찾기).cpp
@@ -0,0 +1,29 @@
+// 문제 출처: https://www.acmicpc.net/problem/10809
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int i = 0, j = 0;
+    char word[101]; // 문제가 100보다 작다가 아니라 100을 안 넘긴다 여서 100자리를 포함할 수 있게 작성해야함
+    cin >> word;
+
+    for (i=0; i<26; i++)
+    {
+        for(j=0; j<strlen(word); j++)
+        {
+            if (i == (word[j] - 97))
+            {
+                cout << j << " ";
+                break;
+            }
+        }
+        if (j == strlen(word))
+        {
+            cout << -1 << " ";
+        }
+    }
+}
