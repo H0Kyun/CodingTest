ac8c5c0e78feaeae01a5513302daa530b829669e
Hokyun <h0kyungim@gmail.com>
2022-05-20 19:25:20 +0900

    swea 10726

---
+++ SWExpertAcademy/D3/10726(이진수 표현).cpp
@@ -0,0 +1,29 @@
+// 문제 출처: https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXRSXf_a9qsDFAXS
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int testCase, T;
+
+    cin >> T;
+
+    for(testCase = 1; testCase <= T; testCase++)
+    {
+        int numberOfStudents;
+        int numberOfTeams;
+
+        cin >> numberOfStudents;
+
+        numberOfTeams = numberOfStudents / 3;
+        
+        cout << "#" << testCase << " " << numberOfTeams << "\n";
+    }
+
+    return 0;
+}
