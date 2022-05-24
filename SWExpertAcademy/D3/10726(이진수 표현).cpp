6d58fcc517eff94d5d8ae0a681638ae3e2a655d5
h0kyun <h0kyungim@gmail.com>
2022-05-24 15:48:27 +0900

    solve 10726

--- SWExpertAcademy/D3/10726(이진수 표현).cpp
+++ SWExpertAcademy/D3/10726(이진수 표현).cpp
@@ -1,22 +1,33 @@
 // 문제 출처: https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXRSXf_a9qsDFAXS
 
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	int testCase, T;
 
-    int testCase, T;
+	cin >> T;
 
-    cin >> T;
+	for (testCase = 1; testCase <= T; testCase++)
+	{
+		int number, inputBit;
+        int compareBit = 1;
 
-    for(testCase = 1; testCase <= T; testCase++)
-    {
-        
-    }
+		cin >> number >> inputBit;
+		for (int i = 1; i < number; i++)
+		{
+			compareBit += (int)pow(2, i);
+		}
 
-    return 0;
+		int bitOperationResult = compareBit & inputBit;
+
+		if (bitOperationResult == compareBit)
+			cout << "#" << testCase << " ON\n";
+		else
+			cout << "#" << testCase << " OFF\n";
+	}
+	return 0;
 }
