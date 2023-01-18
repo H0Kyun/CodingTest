0d74b958fedce9b7bacef58eaa05604611172779
김호균 <H0KyunGim@gmail.com>
2023-01-19 01:17:07 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 4.11 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -6,45 +6,24 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> answer = new ArrayList<>();
-        String[] yearMonthDay = today.split("\\.");
+        int todayDays = getTotalDays(today);
         
-        Map<String, Integer> termsMap = new HashMap<>();
+        Map<String, Integer> periodOfTerms = new HashMap<>();
         for(String s : terms) {
-            String[] sArray = s.split(" ");
-            termsMap.put(sArray[0], Integer.parseInt(sArray[1]));
+            String[] termsAndPeriod = s.split(" ");
+            periodOfTerms.put(termsAndPeriod[0], Integer.parseInt(termsAndPeriod[1]));
         }
         
         for(int i = 0; i < privacies.length; i++) {
-            String[] sArray = privacies[i].split(" ");
-            String[] date = sArray[0].split("\\.");
-            int term = termsMap.get(sArray[1]);
-            int year = Integer.parseInt(date[0]) + term / 12;
-            int month = Integer.parseInt(date[1]) + term % 12;
-            int day = Integer.parseInt(date[2]) - 1;
-            if(month > 12) {
-                year++;
-                month -= 12;
-            }
-            
-            if(day == 0) {
-                day = 28;
-                month--;
-                if(month == 0) {
-                    month = 12;
-                    year--;
-                }
-            }
-            
-            System.out.println(year + " " + month + " " + day)
-            ;
-            if(year < Integer.parseInt(yearMonthDay[0]))
-                answer.add(i + 1);
-            else if(year <= Integer.parseInt(yearMonthDay[0]) && month < Integer.parseInt(yearMonthDay[1]))
-                answer.add(i + 1);
-            else if(year <= Integer.parseInt(yearMonthDay[0]) && month <= Integer.parseInt(yearMonthDay[1]) && day < Integer.parseInt(yearMonthDay[2]))
-                answer.add(i + 1);
+            String[] dateAndTerms = privacies[i].split(" ");
+            if(getTotalDays(dateAndTerms[0]) + periodOfTerms.get(dateAndTerms[1]) * 28 <= todayDays)
+                    answer.add(i + 1);
         }
-        
         return answer.stream().mapToInt(i->i).toArray();
+    }
+    
+    private int getTotalDays(String dateFormat) {
+        String[] date = dateFormat.split("\\.");
+        return Integer.parseInt(date[0]) * 12 * 28 + Integer.parseInt(date[1]) * 28 + Integer.parseInt(date[2]);
     }
 }
