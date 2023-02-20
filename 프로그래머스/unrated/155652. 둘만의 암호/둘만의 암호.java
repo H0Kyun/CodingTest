6b37b3984d5df0d83a965b77d6cfa2eec0160680
김호균 <H0KyunGim@gmail.com>
2023-02-20 16:30:29 +0900

    [unrated] Title: 둘만의 암호, Time: 2.15 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -4,14 +4,17 @@ class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
         
-        // 1증가 하고 검사하고 1증가하고 검사하고?
+        
         for(int i = 0; i < s.length(); i++) {
             int idx = 0;
             char temp = s.charAt(i);
+            /* 1증가 하고 검사하고 반복
+               while문을 생각하지 못 해 코드가 참 이상했다 */
             while(idx < index) {
                 temp++;
                 if(temp > 'z') temp = 'a';
                 
+                // skip도 문자 배열로 만들어 루프 돌렸는데 그럴필요없었다.
                 if(!skip.contains(Character.toString(temp)))
                     idx++;
             }
