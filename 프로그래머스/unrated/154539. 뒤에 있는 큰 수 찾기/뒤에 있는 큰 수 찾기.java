8c88777b4cae3f9968832c483171feb3596d63f7
김호균 <H0KyunGim@gmail.com>
2023-04-12 23:12:58 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 35.59 ms, Memory: 192 MB -BaekjoonHub

--- 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -7,17 +7,22 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         for(int i = numbers.length - 1; i >= 0; i--) {
-            
             while(!pq.isEmpty()) {
+                
+                // 최소 힙에 있는 루트 값이 현재 숫자보다 크면
+                // 루트 값을 답으로 넣고 힙에 현재 숫자를 추가한다.
                 if(pq.peek() > numbers[i]) {
                     answer[i] = pq.peek();
                     pq.add(numbers[i]);
                     break;
                 } else {
+                    // 그게 아니면 최소힙의 루트를 제거한다.
                     pq.remove();
                 }
             }
             
+            // 최소힙이 비어있으면 나보다 큰 수가 없다는 뜻이므로
+            // -1을 답으로 넣고 힙에 나를 추가한다.
             if(pq.isEmpty()) {
                 answer[i] = -1;
                 pq.add(numbers[i]);
