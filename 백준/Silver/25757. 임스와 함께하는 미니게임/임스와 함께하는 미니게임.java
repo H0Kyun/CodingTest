36e08b3a5d00d96f914a96ddecc1cf27b541fab7
김호균 <H0KyunGim@gmail.com>
2023-06-05 09:23:22 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 760 ms, Memory: 291952 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -7,18 +7,23 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int numberOfMatchRequests = sc.nextInt();
         String matchType = sc.next();
+        
         Set<String> userNames = new HashSet<>();
         sc.nextLine();
         for(int i = 1; i <= numberOfMatchRequests; i++) {
             userNames.add(sc.nextLine());
         }
         
-        if (matchType.equals("Y")) {
-            System.out.println(userNames.size());
-        } else if(matchType.equals("F")) {
-            System.out.println(userNames.size() / 2);
-        } else if(matchType.equals("O")) {
-            System.out.println(userNames.size() / 3);
+        switch (matchType) {
+            case "Y":
+                System.out.println(userNames.size());
+                break;
+            case "F":
+                System.out.println(userNames.size() / 2);
+                break;
+            case "O":
+                System.out.println(userNames.size() / 3);
+                break;
         }
         
         sc.close();
