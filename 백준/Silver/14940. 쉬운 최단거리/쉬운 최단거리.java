1199d2305d035c1f9d4f0cbcd17bb20dbdbce618
김호균 <H0KyunGim@gmail.com>
2023-06-14 17:31:30 +0900

    Update 쉬운 최단거리.java

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -14,14 +14,15 @@ class Main {
         }
 
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int height = sc.nextInt();
         int wide = sc.nextInt();
         sc.nextLine();
 
-        int[][] originalBoard = new int[height][wide];
-        int[][] finalBoard = new int[height][wide];
+        int[][] originalBoard = new int[height][wide]; // 입력받은 지도
+        int[][] finalBoard = new int[height][wide]; // 최종 결과를 저장할 배열
         
         int startY = 0;
         int startX = 0;
@@ -31,7 +32,7 @@ class Main {
 
             for (int j = 0; st.hasMoreTokens(); j++) {
                 originalBoard[i][j] = Integer.parseInt(st.nextToken());
-                if(originalBoard[i][j] == 2) {
+                if(originalBoard[i][j] == 2) { // 값이 2이면 시작지점이다.
                     startY = i;
                     startX = j;
                 }
@@ -44,7 +45,7 @@ class Main {
         boolean[][] visited = new boolean[height][wide];
         visited[startY][startX] = true;
 
-        while(!queue.isEmpty()) {
+        while(!queue.isEmpty()) { // depth를 계산하는 bfs
             int size = queue.size();
 
             while(--size >= 0) {
@@ -61,7 +62,14 @@ class Main {
                     int ny = y + dy[d];
                     int nx = x + dx[d];
 
-                    if(ny < 0 || nx < 0 || ny >= height || nx >= wide || originalBoard[ny][nx] == 0 || visited[ny][nx]) continue;
+                    if(
+                        ny < 0 || nx < 0 
+                        || ny >= height || nx >= wide 
+                        || originalBoard[ny][nx] == 0 || visited[ny][nx]
+                      ) {
+                        continue;
+                    }
+                    
                     visited[ny][nx] = true;
                     queue.add(new Position(ny, nx));
                 }
@@ -72,6 +80,10 @@ class Main {
 
         for(int i = 0; i < height; i++) {
             for(int j = 0; j < wide; j++) {
+                /* 
+                 * 결과값은 0이지만 지도의 값은 1이면 도달 못 했다는 뜻이므로
+                 * 결과값을 -1로 변경한다.
+                 */
                 if(finalBoard[i][j] == 0 && originalBoard[i][j] == 1) {
                     finalBoard[i][j] = -1;
                 }
