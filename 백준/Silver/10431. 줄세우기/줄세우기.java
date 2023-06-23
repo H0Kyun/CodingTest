72da166c9ba32ae7f56e2235842b7189320d4615
김호균 <H0KyunGim@gmail.com>
2023-06-23 17:20:42 +0900

    Update 줄세우기.java

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -10,13 +10,14 @@ public class Main {
             int testCaseNum = sc.nextInt();
             
             List<Integer> students = new ArrayList<>();
+            int backMovingCount = 0;
             
+            /* 앞에서부터 정렬하기 때문에 이제 줄을 서야하는 학생(i 번째 학생)보다
+             * 앞에 선 키가 작은 사람은 이미 본인보다 큰 사람 앞으로 이동을 마친 상태이므로
+             * i 번째 학생보다 키가 큰사람만 뒤로 움직이게 된다.
+             * 결국 i 번째 학생보다 앞에 있으며 키가 큰 사람의 수만 카운트하면 된다. */
             for(int i = 0; i < 20; i++) {
                 students.add(sc.nextInt());
-            }
-            
-            int backMovingCount = 0;
-            for(int i = 0; i < 20; i++) {
                 for(int j = 0; j < i; j++) {
                     if(students.get(i) < students.get(j)) {
                         backMovingCount++;
