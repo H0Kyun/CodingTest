02790ad192eaefe2d87730e4dd884595aaeb0cc9
김호균 <H0KyunGim@gmail.com>
2023-07-19 21:14:40 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    long long A = 0;
+    long long B = 0;
+    long long C = 0;
+    
+    cin >> A >> B >> C;
+    
+    cout << A + B + C;
+    
+    return 0;
+}
