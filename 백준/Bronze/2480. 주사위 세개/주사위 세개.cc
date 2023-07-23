840d6d361a36dd830e3c30af2e91b5d11b55023a
김호균 <H0KyunGim@gmail.com>
2023-07-23 12:14:51 +0900

    Update 주사위 세개.cc

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -17,19 +17,22 @@ int main()
         }
     }
     
+    // 셋이 모두 같은 경우
     if (diceValues[0] == diceValues[1] && diceValues[1] == diceValues[2])
     {
         cout << 10000 + diceValues[0] * 1000;
     } 
-    else if (diceValues[0] != diceValues[1] && diceValues[1] == diceValues[2])
+    // 앞의 if 문에서 안 걸렸다는 것은 셋 중 하나가 다르거나 모두 다르다는 의미이므로
+    // 둘이 같은 것이 있는지만 확인한다.
+    else if (diceValues[0] == diceValues[1] || diceValues[1] == diceValues[2])
     {
         cout << 1000 + diceValues[1] * 100;
     }
-    else if ((diceValues[0] == diceValues[1] && diceValues[1] != diceValues[2])
-        ||(diceValues[0] != diceValues[1] && diceValues[0] == diceValues[2]))
+    else if (diceValues[0] == diceValues[2])
     {
         cout << 1000 + diceValues[0] * 100;
     }
+    // 모두 다른 경우
     else
     {
         cout << max * 100;
