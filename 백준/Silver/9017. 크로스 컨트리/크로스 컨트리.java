1da9d3a158eb32c392f5f73bf42e971b54f68d34
김호균 <H0KyunGim@gmail.com>
2023-07-23 13:45:15 +0900

    Update 크로스 컨트리.java

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
@@ -4,23 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-        // 1. 각 팀의 인원을 저장한다. 팀이 6명인지 확인한다.
-        // 2. 6명이 아닌 팀이 있으면 해당 팀 선수들에게는 점수를 주지 않는다.
-        // 3. 점수를 계산한다. 계산하면서 5번째 선수의 점수는 따로 저장한다.
-        // 4. 같은 점수의 팀이 존재하면 미리 저장한 5번째 선수의 점수를 비교한다.
         
         int T = sc.nextInt();
-        
         
         for(int testCase = 1; testCase <= T; ++testCase) {
             sc.nextLine();
             int N = sc.nextInt();
             sc.nextLine();
             
-            int[] numberOfTeamMember = new int[201];
-            int[] scoreOfTeam = new int[201];
-            int[] scoreOfFifthPlayer = new int[201];
-            int[] raceResult = new int[N];
+            int[] numberOfTeamMember = new int[201]; // 팀 인원 수
+            int[] scoreOfTeam = new int[201]; // 팀 점수
+            int[] scoreOfFifthPlayer = new int[201]; // 팀별 다섯 번째 선수 점수
+            int[] raceResult = new int[N]; // 경기 결과
             
             for(int i = 0; i < N; ++i) {
                 int teamNumber = sc.nextInt();
@@ -36,13 +31,15 @@ public class Main {
                 }
                 
                 ++score;
-                //System.out.print(raceResult[i] + ": " + score + "/ ");
-                ++numberOfTeamMember[raceResult[i]];
+                // 다섯 번째 선수를 찾는 목적으로 팀인원수를 늘린다.
+                ++numberOfTeamMember[raceResult[i]]; 
                 
+                // 팀 인원수가 11이 되면 해당 팀의 다섯 번째 선수를 의미한다.
                 if(numberOfTeamMember[raceResult[i]] == 11) {
                     scoreOfFifthPlayer[raceResult[i]] = score;
                 }
                 
+                // 점수는 팀 선두 네 명의 점수 합이므로 네 명까지만 더한다.
                 if(numberOfTeamMember[raceResult[i]] < 11) {
                     scoreOfTeam[raceResult[i]] += score;
                 }
@@ -51,16 +48,17 @@ public class Main {
             int minScore = Integer.MAX_VALUE;
             int winner = 0;
             for(int i = 1; i < 201; ++i) {
-                if(scoreOfTeam[i] <= 0) {
+                // 팀 점수가 0이면 참가하지 않은 팀이거나 인원이 부족한 팀이므로 넘어간다.
+                if(scoreOfTeam[i] == 0) {
                     continue;
                 }
                 
+                // 최소 점수인 팀이 우승팀이다.
                 if(scoreOfTeam[i] < minScore) {
-                    
                     minScore = scoreOfTeam[i];
                     winner = i;
                 } else if(scoreOfTeam[i] == minScore) {
-            
+                    // 점수가 같으면 다섯 번째 선수의 점수를 비교해 더 작은 팀이 우승팀이 된다.
                     if (scoreOfFifthPlayer[winner] > scoreOfFifthPlayer[i]) {
                         winner = i;
                     }
@@ -70,7 +68,6 @@ public class Main {
             sb
                 .append(winner)
                 .append("\n");
-            
         }
         
         System.out.println(sb);
