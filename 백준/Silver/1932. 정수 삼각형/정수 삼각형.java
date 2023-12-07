4253319d601b235e2bf8aab6abd1f4765194014c
김호균 <H0KyunGim@gmail.com>
2023-12-07 21:01:25 +0900

    [Silver I] Title: 정수 삼각형, Time: 220 ms, Memory: 23840 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -4,13 +4,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    private int n;
+    private int[][] tree;
+    private int[][] dp;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Main solution = new Main();
+        solution.input();
+        System.out.println(solution.getMaximumSum());
+    }
 
-        int[][] tree = new int[n][];
-        int[][] dp = new int[n][];
+    private int getMaximumSum() {
+        dp[n - 1] = tree[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = 0; j < dp[i].length; ++j) {
+                dp[i][j] = Math.max(tree[i][j] + dp[i + 1][j + 1],
+                    Math.max(dp[i][j], tree[i][j] + dp[i + 1][j]));
+            }
+        }
+        
+        return dp[0][0];
+    }
+
+    private void input() {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            n = Integer.parseInt(br.readLine());
+            tree = new int[n][];
+            dp = new int[n][];
+
+            createTree(br);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createTree(BufferedReader br) throws IOException {
         for (int i = 0; i < n; ++i) {
             tree[i] = new int[i + 1];
             dp[i] = new int[i + 1];
@@ -20,16 +48,5 @@ public class Main {
                 tree[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        br.close();
-
-        dp[n - 1] = tree[n - 1];
-        for (int i = n - 2; i >= 0; --i) {
-            for (int j = 0; j < dp[i].length; ++j) {
-                dp[i][j] = Math.max(tree[i][j] + dp[i + 1][j + 1],
-                    Math.max(dp[i][j], tree[i][j] + dp[i + 1][j]));
-            }
-        }
-
-        System.out.println(dp[0][0]);
     }
 }
