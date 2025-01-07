1a844985533d40b9539aaac9f709c58698d1b86c
김호균 <H0KyunGim@gmail.com>
2025-01-08 01:10:42 +0900

    [Silver III] Title: N번째 큰 수, Time: 276 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -7,21 +7,28 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); 
     
-    int N = 1;
+    int N;
     
     cin >> N;
     
     priority_queue<int, vector<int>, greater<int>> pq;
     
-    for (int i = 0; i < N * N; ++i) {
+    for (int i = 0; i < N; ++i) {
         int num = 0;
         
         cin >> num;
         
         pq.push(num);
+    }
+    
+    for (int i = N; i < N * N; ++i) {
+        int num = 0;
         
-        if (pq.size() > N) {
+        cin >> num;
+        
+        if (pq.top() < num) {
             pq.pop();
+            pq.push(num);
         }
     }
     
