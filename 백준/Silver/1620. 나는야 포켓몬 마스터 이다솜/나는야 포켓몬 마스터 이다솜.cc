13ed45fd04b8908669feaf057925fff4cf6054d4
김호균 <H0KyunGim@gmail.com>
2025-02-02 15:41:27 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 376 ms, Memory: 29924 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+int main() {
+    unsigned int n = 0;
+    unsigned int m = 0;
+
+    cin >> n >> m;
+
+    map<string, string> number_book;
+    map<string, unsigned int> name_book;
+
+
+    for (unsigned int i = 1; i <= n; ++i) {
+        string name;
+        cin >> name;
+
+        number_book.insert(make_pair(to_string(i), name));
+        name_book.insert(make_pair(name, i));
+    }
+
+    string result;
+
+    for (unsigned int i = 0; i < m; ++i) {
+        string query;
+
+        cin >> query;
+
+        if (number_book.find(query) != number_book.end()) {
+            result += number_book.find(query)->second + "\n";
+        } else {
+            result += to_string(name_book.find(query)->second) + "\n";
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
