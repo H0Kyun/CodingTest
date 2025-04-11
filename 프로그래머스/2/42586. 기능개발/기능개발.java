97bf28194bd9364a1e1e32429a020dec47203ff5
김호균 <H0KyunGim@gmail.com>
2025-04-11 16:57:30 +0900

    [level 2] Title: 기능개발, Time: 2.73 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList();
+        
+        int index = -1;
+        int numberOfProject = 0;
+        int previousCountdown = 0;
+        
         int[] deploymentCountdowns = new int[progresses.length];
         
         
@@ -13,21 +19,12 @@ class Solution {
                 deployCountdown++;
             }
             
-            deploymentCountdowns[i] = deployCountdown;
-        }
-        
-        List<Integer> answer = new ArrayList();
-        
-        int index = -1;
-        int numberOfProject = 0;
-        int previousCountdown = 0;
-        
-        for (int countdown : deploymentCountdowns) {
+            // deploymentCountdowns[i] = deployCountdown;
             
-            if (countdown > previousCountdown) {
+            if (deployCountdown > previousCountdown) {
                 ++index;
                 numberOfProject = 0;
-                previousCountdown = countdown;
+                previousCountdown = deployCountdown;
             }
             
             if (index < answer.size()) {
@@ -35,8 +32,23 @@ class Solution {
             } else {
                 answer.add(++numberOfProject);
             }
-            
         }
+        
+//         for (int countdown : deploymentCountdowns) {
+            
+//             if (countdown > previousCountdown) {
+//                 ++index;
+//                 numberOfProject = 0;
+//                 previousCountdown = countdown;
+//             }
+            
+//             if (index < answer.size()) {
+//                 answer.set(index, ++numberOfProject);
+//             } else {
+//                 answer.add(++numberOfProject);
+//             }
+            
+//         }
         
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
