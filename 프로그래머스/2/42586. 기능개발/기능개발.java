95085413b0fbc38f3e4e93966546e12dd28e28aa
김호균 <H0KyunGim@gmail.com>
2025-04-13 19:38:56 +0900

    [level 2] Title: 기능개발, Time: 5.81 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -8,9 +8,6 @@ class Solution {
         int numberOfProject = 0;
         int previousCountdown = 0;
         
-        int[] deploymentCountdowns = new int[progresses.length];
-        
-        
         for (int i = 0; i < progresses.length; ++i) {
             int remainProgress = 100 - progresses[i];
             int deployCountdown = remainProgress / speeds[i];
@@ -18,8 +15,6 @@ class Solution {
             if (remainProgress % speeds[i] > 0) {
                 deployCountdown++;
             }
-            
-            // deploymentCountdowns[i] = deployCountdown;
             
             if (deployCountdown > previousCountdown) {
                 ++index;
@@ -33,22 +28,6 @@ class Solution {
                 answer.add(++numberOfProject);
             }
         }
-        
-//         for (int countdown : deploymentCountdowns) {
-            
-//             if (countdown > previousCountdown) {
-//                 ++index;
-//                 numberOfProject = 0;
-//                 previousCountdown = countdown;
-//             }
-            
-//             if (index < answer.size()) {
-//                 answer.set(index, ++numberOfProject);
-//             } else {
-//                 answer.add(++numberOfProject);
-//             }
-            
-//         }
         
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
