0ce884235fe782b79da4e5f534dba31bcfae553c
김호균 <H0KyunGim@gmail.com>
2025-04-27 18:36:26 +0900

    [level 2] Title: H-Index, Time: 0.74 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,19 +4,12 @@ class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
         
-        int answer = 0;
-        int h = Integer.MAX_VALUE;
-        
-        for(int i = citations.length - 1; i >= 0; i--) {
-            if(citations[i] < h) {
-                h = citations[i];
+        for(int i = citations.length - 1; i >= 0; i--) {        
+            if(citations[i] < citations.length - i)  {
+                return citations.length - i - 1;
             }
-            
-            if(h < citations.length - i) break;
-            
-            answer++;
         }
         
-        return answer;
+        return citations.length;
     }
 }
