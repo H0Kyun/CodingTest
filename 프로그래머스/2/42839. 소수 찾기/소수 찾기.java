e2eb1079885360446de720d849549aeeed6292d2
김호균 <H0KyunGim@gmail.com>
2025-05-04 02:05:22 +0900

    [level 2] Title: 소수 찾기, Time: 99.09 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String numbers) {
+        Set<Integer> primeNumbers = new HashSet();
+        for(int i = 0; i < numbers.length(); ++i) {
+            dfs(i, new boolean[numbers.length()], new StringBuilder().append(numbers.charAt(i)), numbers, primeNumbers);
+        }
+        
+        return primeNumbers.size();
+    }
+    
+    private void dfs(int index, boolean[] visited, StringBuilder currentNumber, String numbers, Set<Integer> primeNumbers) {
+        
+        visited[index] = true;
+        int number = Integer.parseInt(currentNumber.toString());
+        
+        if (isPrimeNumber(number)) {
+            primeNumbers.add(number);
+        }
+        
+        for(int i = 0; i < numbers.length(); ++i) {
+            if (visited[i]) {
+                continue;
+            }
+            
+            dfs(i, Arrays.copyOf(visited, visited.length), new StringBuilder(currentNumber.toString()).append(numbers.charAt(i)), numbers, primeNumbers);
+        }
+    }
+    
+    private boolean isPrimeNumber(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i < number; ++i) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
