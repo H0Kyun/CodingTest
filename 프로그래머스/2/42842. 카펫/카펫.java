ccb5cd80183b4862ac35ab29f35a5a69c5d395a5
김호균 <H0KyunGim@gmail.com>
2025-05-04 23:09:29 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+        // 2 * 가로 + 2 * (세로 - 2)
+        // 가로 * 2 는 위아래, 2 * (세로 - 2)는 좌우 -2 하는 이유는 위아래에서 구한 값을 빼기 위함
+        int sumOfWidthAndhHeight = (brown + 4) / 2;
+        int productOfWidthAndhHeight = brown + yellow; // 전체 면적
         
-        int block = brown / 2 + 2;
-        
-        for (int i = 3; i <= block - i; ++i) {
-            if ((block - i - 2) * (i - 2) == yellow) {
-                answer[0] = block - i;
-                answer[1] = i;
-                
-                return answer;
-            }
-        }
+        return rootFormulas(sumOfWidthAndhHeight, productOfWidthAndhHeight);
+    }
+    
+    public int[] rootFormulas(int a, int b) {
+        // 합과 곱을 알면 이차방정식으로 근을 구할 수 있음
+        // (x - alpha)(x - beta) = 0
+        // x^2 - (합)x + 곱 = 0
+        int discriminant = (int)Math.sqrt(a * a - 4 * b);
+        int[] answer = {(a + discriminant) / 2, (a - discriminant) / 2};
         
         return answer;
     }
