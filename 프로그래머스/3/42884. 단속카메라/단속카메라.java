ca98184340f109ab0cc54968f1c3f055d4656e85
김호균 <H0KyunGim@gmail.com>
2025-05-11 19:04:53 +0900

    [level 3] Title: 단속카메라, Time: 9.10 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -6,17 +6,17 @@ class Solution {
         Arrays.sort(routes, (o1, o2) -> Integer.compare(o1[1], o2[1]));
         
         // 제일 먼저 나간 차량 위치에 카메라 설치
-        int cameraPos = routes[0][1];
+        int cameraPosition = routes[0][1];
         int answer = 1;
         
-        for (int i = 1; i < routes.length; i++) {
+        for (int[] entryAndExistRecords : routes) {
             // 차량이 들어오고 나간 시점 사이에 카메라가 있는 경우
-            if (routes[i][0] <= cameraPos && routes[i][1] >= cameraPos) {
+            if (entryAndExistRecords[0] <= cameraPosition && entryAndExistRecords[1] >= cameraPosition) {
                 continue;
             }
             
             // 첫 차량 나가고 난 후 들어온 차량이 나간 위치에 카메라 설치 
-            cameraPos = routes[i][1];
+            cameraPosition = entryAndExistRecords[1];
             ++answer;
         }
         
