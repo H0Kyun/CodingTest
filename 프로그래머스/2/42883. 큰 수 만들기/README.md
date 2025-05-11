62fd99e94c10957d8789ba846607911508be6b8e
김호균 <H0KyunGim@gmail.com>
2025-05-11 23:12:47 +0900

    [level 2] Title: 큰 수 만들기, Time: 186.54 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#) 
 
 ### 성능 요약
 
-메모리: 99.4 MB, 시간: 182.28 ms
+메모리: 83.9 MB, 시간: 186.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 11일 23:07:05
+2025년 05월 11일 23:12:45
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -3,15 +3,14 @@ import java.util.*;
 class Solution {
     public String solution(String number, int k) {
         StringBuilder answer = new StringBuilder(number);
-        // K + 1까지 반복을 해서 가장 큰 수를 찾는다.
-        // 앞자리가 가장 큰 수가 될 수 있게 수를 제거한다.
-        // 위 과정을 반복해야 함!
+    
         int start = 0;
         
         while(k > 0) {
-            char maxNumber = '-';
+            char maxNumber = '-'; // '0'이 문자열 안에 있으면 maxNumber가 갱신 안 되는 문제 해결
             int maxIndex = 0;
             
+            // 시작 지점부터 K 까지 반복을 해서 가장 큰 수의 위치를 찾는다.
             for (int i = start; i <= k + start; ++i) {
                 if (answer.charAt(i) > maxNumber) {
                     maxNumber = answer.charAt(i);
@@ -26,10 +25,13 @@ class Solution {
             
            
             k -= maxIndex - start;
+            // 앞자리가 가장 큰 수가 될 수 있게 수를 제거한다.
             answer.delete(start, maxIndex);
             
+            // 시작 지점을 하나 민다.
             ++start;
             
+            // 남은 수가 k 보다 작거나 같으면 시작 지점 이후 숫자를 모두 지운다.
             if (k >= answer.length() - start) {
                 answer.delete(start, answer.length());
                 break;
