fbdd37e999061c8c8dbf5cf5de1da48bc6f3afd5
김호균 <H0KyunGim@gmail.com>
2025-05-25 20:09:12 +0900

    [level 2] Title: 구명보트, Time: 11.04 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -7,27 +7,13 @@ class Solution {
         int answer = 0;
         int index = people.length - 1;
         
-        for (int i = 0; i < people.length; ++i) {  
-            if (people[i] == 0) {
-                continue;
-            }
-            
-            if (people[index] == 0) {
-                break;
-            }
-                
-            int sum = people[i] + people[index];
-
-            if (sum <= limit) {
-                people[i] = 0;
-                people[index] = 0;
-                --index;
-            } else {
-                people[index] = 0;
-                --index;
+        for (int i = 0; i <= index; ++i) {
+            if (people[i] + people[index] > limit) {
                 --i;
             }
             
+            --index;
+
             ++answer;
         }
         
