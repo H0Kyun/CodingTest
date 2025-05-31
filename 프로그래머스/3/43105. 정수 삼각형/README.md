5a5004cbcee7ed6e82f5e83ba111473dccbdcedb
김호균 <H0KyunGim@gmail.com>
2025-06-01 02:48:07 +0900

    [level 3] Title: 정수 삼각형, Time: 11.67 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java) 
 
 ### 성능 요약
 
-메모리: 63.1 MB, 시간: 11.50 ms
+메모리: 64 MB, 시간: 11.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 01일 02:39:54
+2025년 06월 01일 02:48:07
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,14 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        
-        for (int i = triangle.length - 2; i >= 0; --i) {
-            for(int j = 0; j < triangle[i].length; ++j) {
-                triangle[i][j] = (triangle[i][j] + Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]));
-            }
+        int n = triangle.length;
+    int[][] dp = new int[n][n];
+
+    dp[0][0] = triangle[0][0];
+
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = dp[i-1][0] + triangle[i][0];  // 왼쪽 끝
+        for (int j = 1; j < i; j++) {
+            dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];  // 중간
         }
-        
-        return triangle[0][0];
+        dp[i][i] = dp[i-1][i-1] + triangle[i][i];  // 오른쪽 끝
+    }
+
+    int max = 0;
+    for (int val : dp[n-1]) {
+        max = Math.max(max, val);
+    }
+    return max;
     }
 }
