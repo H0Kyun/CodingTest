bd5be923f69507eaf35e1aa0f8462f4d9db57c9b
김호균 <H0KyunGim@gmail.com>
2025-06-15 18:49:35 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 8.17 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -1,44 +1,59 @@
 import java.util.*;
 
 class Point {
-        int y;
-        int x;
-        
-        Point(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
+    int y;
+    int x;
+
+    Point(int y, int x) {
+        this.y = y;
+        this.x = x;
     }
+}
 
 class Block {
-    int[][][] tables = new int[4][][];
+    int[][] table;
     int size;
     
     Block(int size, int[][] table) {
         this.size = size;
-        this.tables[0] = table;
-        createTables();
+        this.table = table;
     }
     
-    private void createTables() {
-        for (int i = 1; i < 4; ++i) {
-            // tables[i - 1] 90도 회전 값 넣기
-            this.tables[i] = rotateTable(this.tables[i - 1]);
+    public boolean compare(Block block) {
+        if (this.size != block.size) {
+            return false;
         }
         
-        // System.out.println("Block");
-        // for (int[][] table : this.tables) {
-        //     for (int[] column : table) {
-        //         for (int data : column) {
-        //             System.out.printf("%d ", data);
-        //         }
-        //         System.out.println();
-        //     }
-        //     System.out.println();
-        // }
+        int[][] tableToCompare = this.table;
+        
+        for (int i = 0; i < 4; ++i) {
+            if(compareTable(tableToCompare, block.table)) {
+                return true;
+            }
+            
+            tableToCompare = rotate(tableToCompare);
+        }
+        
+        return false;
     }
     
-    private int[][] rotateTable(int[][] table) {
+    private boolean compareTable(int[][] table1, int[][] table2) {
+        if (table1.length != table2.length || table1[0].length != table2[0].length) {
+            return false;
+        }
+        
+        for (int i = 0; i < table1.length; ++i) {
+            for (int j = 0; j < table1[0].length; ++j) {
+                if (table1[i][j] != table2[i][j]) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    private int[][] rotate(int[][] table) {
         int row = table.length;
         int col = table[0].length;
 
@@ -52,149 +67,128 @@ class Block {
         
         return rotatedTable;
     }
+}
+
+class Solution {
+    static class CropInformation {
+        int minY;
+        int minX;
+        int maxY;
+        int maxX;
+        
+        CropInformation(int minY, int minX, int maxY, int maxX) {
+            this.minY = minY;
+            this.minX = minX;
+            this.maxY = maxY;
+            this.maxX = maxX;
+        }
+    }
     
-    public boolean compareTable(int[][] table) {
-        for (int t = 0; t < this.tables.length; ++t) {
-            boolean isSame = true;
-            
-            if (this.tables[t].length != table.length || this.tables[t][0].length != table[0].length) {
-                continue;
-            }
-            
-            for (int i = 0; i < this.tables[t].length; ++i) {
-                for (int j = 0; j < this.tables[t][i].length; ++j) {
-                    if (this.tables[t][i][j] == table[i][j]) {
-                        isSame = false;
-                        break;
-                    }
-                }
-                
-                if (!isSame) {
+    public int solution(int[][] game_board, int[][] table) {    
+        List<Block> tableBlocks = findBlock(table, 1);
+        List<Block> gameBoardBlocks = findBlock(game_board, 0);
+
+        int answer = 0;
+        
+        for (Block gameBoardBlock : gameBoardBlocks) {
+            for (Block tableBlock : tableBlocks) {
+                if (gameBoardBlock.compare(tableBlock)) {
+                    answer += gameBoardBlock.size;
+                    
+                    tableBlocks.remove(tableBlock);
+                    
                     break;
                 }
             }
             
-            if (isSame) {
-                return isSame;
-            }
-        }
-        
-        return false;
-    }
-}
-
-class Solution {
-    public int solution(int[][] game_board, int[][] table) {    
-        int answer = 0;
-        List<Block> list = new ArrayList<Block>();
-        // table 탐색 조각(1)만나면 탐색
-        boolean[][] visited = new boolean[table.length][table[0].length];
-        for (int i = 0; i < table.length; ++i) {
-            for (int j = 0; j < table[i].length; ++j) {
-                if (table[i][j] == 1 && !visited[i][j]) {
-                    // 탐색 하면서 최소 j값, 최대 j, 최고 i 값 저장
-                    int[] coordinate = bfs(new Point(i, j), table.length, table[0].length, visited, table, 0);
-                    // 위 결과로 찾아낸 좌표를 이용해서 테이블 복사 가능
-                    int[][] copied = new int[coordinate[2] - coordinate[0] + 1][coordinate[3] - coordinate[1] + 1];
-                    for(int ci = coordinate[0]; ci <= coordinate[2]; ++ci) {
-                        for(int cj = coordinate[1]; cj <= coordinate[3]; ++cj) {
-                            copied[ci - coordinate[0]][cj - coordinate[1]] = table[ci][cj];
-                        }
-                    }
-                    // System.out.println("Copied Block");
-                    // for(int[] column : copied) {
-                    //     for (int data : column) {
-                    //         System.out.printf("%d ", data);
-                    //     }
-                    //     System.out.println();
-                    // }
-                    // 복사한 테이블로 Block객체 생성
-                    list.add(new Block(coordinate[4], copied));
-                }
-            }
-        }
-        // game_board 탐색 조각(0)만나면 탐색
-        visited = new boolean[game_board.length][game_board[0].length];
-        for (int i = 0; i < game_board.length; ++i) {
-            for (int j = 0; j < game_board[i].length; ++j) {
-                if (game_board[i][j] == 0 && !visited[i][j]) {
-                    // 탐색 하면서 최소 j값, 최대 j, 최고 i 값 저장
-                    int[] coordinate = bfs(new Point(i, j), game_board.length, game_board[0].length, visited, game_board, 1);
-                    // 위 결과로 찾아낸 좌표를 이용해서 테이블 복사 가능
-                    int[][] copied = new int[coordinate[2] - coordinate[0] + 1][coordinate[3] - coordinate[1] + 1];
-                    for(int ci = coordinate[0]; ci <= coordinate[2]; ++ci) {
-                        for(int cj = coordinate[1]; cj <= coordinate[3]; ++cj) {
-                            copied[ci - coordinate[0]][cj - coordinate[1]] = game_board[ci][cj];
-                        }
-                    }
-                    
-                    // System.out.println("Found Block");
-                    // for(int[] column : copied) {
-                    //     for (int data : column) {
-                    //         System.out.printf("%d ", data);
-                    //     }
-                    //     System.out.println();
-                    // }
-                    // 복사한 테이블로 Block객체 생성
-                    for (Block block : list) {
-                        if (block.size == coordinate[4] && block.compareTable(copied)) {
-                            answer += block.size;
-                            list.remove(block);
-                            break;
-                        }
-                    }
-                }
-            }
         }
         
         return answer;
     }
     
-    int[] bfs(Point point, int y, int x, boolean[][] visited, int[][] table, int flag) {
-        //(y,x,y,x)
-        int[] coordinate = new int[5];
-        coordinate[0] = point.y;
-        coordinate[1] = point.x;
-        coordinate[2] = point.y;
-        coordinate[3] = point.x;
-        coordinate[4] = 0;
+    List<Block> findBlock(int[][] table, int target) {
+        List<Block> list = new ArrayList<Block>();
+        
+        int row = table.length;
+        int column = table[0].length;
+        
+        boolean[][] visited = new boolean[row][column];
+        
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < column; ++j) {
+                if (table[i][j] == target && !visited[i][j]) {
+                    // 탐색 하면서 최소 j값, 최대 j, 최고 i 값 저장
+                    CropInformation cropInfo = bfs(new Point(i, j), visited, table, target);
+                    
+                    // 복사한 테이블로 Block객체 생성
+                    list.add(cropTable(cropInfo, table, target));
+                }
+            }
+        }
+        
+        return list;
+    }
+    
+    CropInformation bfs(Point point, boolean[][] visited, int[][] table, int target) {
+        int minY = point.y;
+        int minX = point.x;
+        int maxY = point.y;
+        int maxX = point.x;
+        
+        int row = table.length;
+        int column = table[0].length;
         
         Queue<Point> q = new ArrayDeque<Point>();
         q.offer(point);
-        visited[point.y][point.x] = true;
         
+        visited[point.y][point.x] = true;
         
         int[] dy = { -1, 1, 0, 0};
         int[] dx = { 0, 0, -1, 1};
         
         while(!q.isEmpty()) {
             Point pair = q.poll();
-            ++coordinate[4];
+            
             for(int i = 0; i < 4; ++i) {
                 int ny = pair.y + dy[i]; 
                 int nx = pair.x + dx[i]; 
                 
-                if (ny < 0 || nx < 0 || nx >= x || ny >= y || visited[ny][nx] || table[ny][nx] == flag) {
+                if (ny < 0 || nx < 0 || ny >= row || nx >= column) {
                     continue;
                 }
                 
-                if (nx < coordinate[1]) {
-                    coordinate[1] = nx;
+                if (visited[ny][nx] || table[ny][nx] != target) {
+                    continue;
                 }
                 
-                if (ny > coordinate[2]) {
-                    coordinate[2] = ny;
-                }
-                
-                if (nx > coordinate[3]) {
-                    coordinate[3] = nx;
-                }
+                minY = Math.min(ny, minY);
+                minX = Math.min(nx, minX);
+                maxY = Math.max(ny, maxY);
+                maxX = Math.max(nx, maxX);
                 
                 q.offer(new Point(ny, nx));
+                
                 visited[ny][nx] = true;
             }
         }
         
-        return coordinate;
+        return new CropInformation(minY, minX, maxY, maxX);
+    }
+    
+    Block cropTable(CropInformation cropInfo, int[][] table, int target) {
+        int count = 0;
+        int[][] copied = new int[cropInfo.maxY - cropInfo.minY + 1][cropInfo.maxX - cropInfo.minX + 1];
+        
+        for(int i = cropInfo.minY; i <= cropInfo.maxY; ++i) {
+            for(int j = cropInfo.minX; j <= cropInfo.maxX; ++j) {
+                if (table[i][j] == target) {
+                    ++count;
+                }
+                
+                copied[i - cropInfo.minY][j - cropInfo.minX] = target == 0 ? 1 - table[i][j] : table[i][j];
+            }
+        }
+        
+        return new Block(count, copied);
     }
 }
