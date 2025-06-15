56171a52b51900bb98a07a69d14bace1584f368e
김호균 <H0KyunGim@gmail.com>
2025-06-15 20:59:31 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 14.71 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -33,7 +33,6 @@ class Solution {
         int maxX = query[3] - 1;
         
         int length = (maxX - minX) * 2 + (maxY - minY) * 2;
-        
         int[] dy = {1, 0, -1, 0};
         int[] dx = {0, 1, 0, -1};
         int d = 0;
@@ -41,6 +40,7 @@ class Solution {
         int x = minX;
         int temp = this.table[y][x];
         int minValue = temp;
+        
         for (int i = 0; i < length - 1; ++i) {
             int ny = y + dy[d];
             int nx = x + dx[d];
@@ -59,41 +59,6 @@ class Solution {
         }
         
         table[y][x] = temp;
-         
-        // 위 회전
-//         int last = this.table[minY][maxX];
-//         for (int i = maxX; i > minX; --i) {
-//             this.table[minY][i] = this.table[minY][i - 1];
-//             minValue = Math.min(minValue, this.table[minY][i]);
-//         }
-//         this.table[minY][minX] = this.table[minY + 1][minX];
-//         minValue = Math.min(minValue, this.table[minY][minX]);
-
-//         // 오른쪽 회전
-//         int first = this.table[maxY][maxX];
-//         for (int i = maxY; i > minY + 1; --i) {
-//             this.table[i][maxX] = this.table[i - 1][maxX];
-//             minValue = Math.min(minValue, this.table[i][maxX]);
-//         }
-//         this.table[minY + 1][maxX] = last;
-//         minValue = Math.min(minValue, this.table[minY + 1][maxX]);
-
-//         // 아래쪽 회전
-//         last = this.table[maxY][minX];
-//         for (int i = minX; i < maxX - 1; ++i) {
-//             this.table[maxY][i] = this.table[maxY][i + 1];
-//             minValue = Math.min(minValue, this.table[maxY][i]);
-//         }
-//         this.table[maxY][maxX - 1] = first;
-//         minValue = Math.min(minValue, this.table[maxY][maxX - 1]);
-        
-//         // 왼쪽 회전
-//         for (int i = minY + 1; i < maxY - 1; ++i) {
-//             this.table[i][minX] = this.table[i + 1][minX];
-//             minValue = Math.min(minValue, this.table[i][minX]);
-//         }
-//         this.table[maxY - 1][minX] = last;
-//         minValue = Math.min(minValue, this.table[maxY - 1][minX]);
 
         return minValue;
     }
