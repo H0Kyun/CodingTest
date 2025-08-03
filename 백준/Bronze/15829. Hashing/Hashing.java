4be69185985d7e34f606a731b879741aba93caff
김호균 <H0KyunGim@gmail.com>
2025-08-03 16:12:29 +0900

    Update Hashing.java

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,25 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int stringLength = Integer.parseInt(br.readLine());
         String str = br.readLine();
         long answer = 0;
         long pow = 1;
         long mod = 1234567891;
-        
+
+        // mod 분배 법칙
+        // (a + b) mod m = ((a mod m) + (b mod m)) mod m (덧셈)
+        // (a - b) mod m = ((a mod m) - (b mod m)) mod m (뺄셈)
+        // (a * b) mod m = ((a mod m) * (b mod m)) mod m (곱셈) 
+        /* 예제
+         * (a * 1 + b * 1 * 31 + c * 1 * 31 * 31) % mod
+         * = (a*1 %mode  b * 1 * 31% mod   c * 1 * 31 * 31% mod) % mod
+         * = (a % mod * 1 %mod) %mod + (b % mod * 1 % mod * 31 % mod) % mod
+         */
         for (int i = 0; i < stringLength; ++i) {
             long number = ((((str.charAt(i) - 'a') + 1) % 1234567891) *  pow) % mod;
-            pow = (pow * 31) % 1234567891;
-            
+            pow = (pow * 31) % mod;
+
             answer += number;
         }
         answer %= mod;
-        
+
         System.out.println(answer);
     }
 }
