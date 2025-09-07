0c75c131056e807fd9292cbbcff0eb93ae03e8dc
김호균 <H0KyunGim@gmail.com>
2025-09-07 16:33:52 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1160 ms, Memory: 56640 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Point {
+    int x;
+    int y;
+    
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        PriorityQueue<Point> pq = new PriorityQueue<Point>((p1, p2) -> {
+            if (p1.y == p2.y) {
+                return p1.x - p2.x;
+            } else {
+                return p1.y - p2.y;
+            }
+        });
+        
+        for(int i = 0; i < n; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            
+            pq.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        
+        for(int i = 0; i < n; ++i) {
+            Point point = pq.poll();
+            
+            System.out.println(point.x + " " + point.y);
+        }
+        
+    }
+}
